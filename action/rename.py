a667c36470a66309db5b3ad8999b65ed487e9725
Stevenson Yan <stevensoncodes@gmail.com>
2025-08-22 02:08:29 -0700

    added rename.py

--- action/rename.py
+++ action/rename.py
@@ -8,5 +8,5 @@ def rename_file2(src, dst):
     f = Path(src)
     f.rename(dst)
 
-# rename_file('../_files/text.txt', '../_files/test.txt')
+rename_file('../_files/text.txt', '../_files/test.txt')
 rename_file('../_files/test.txt', '../_files/text.txt')
